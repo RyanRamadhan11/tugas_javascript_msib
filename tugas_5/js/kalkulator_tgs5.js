822cb2bd9ce8f6b2f5a588d36b67bd7900bbf1bc
RyanRamadhan11 <ramadhanryan676@gmail.com>
2022-09-19 20:51:14 +0700

    add tugas 5 js

--- tugas_5/js/kalkulator_tgs5.js
+++ tugas_5/js/kalkulator_tgs5.js
@@ -3,54 +3,54 @@
 // function dengan 1 parameter
 function hitung(paramValue) {
   var formKalkulator = document.getElementById("FormKalkulator_tgs5");
-  var a1 = parseFloat(formKalkulator.angka1.value);
-  var a2 = parseFloat(formKalkulator.angka2.value);
+  var angka1 = parseFloat(formKalkulator.angka1.value);
+  var angka2 = parseFloat(formKalkulator.angka2.value);
 
   // memanggil value button dengan parameter
   var button = paramValue.value;
 
   switch (button) {
     case "button_tambah":
-      if (isNaN(a1) || isNaN(a2)) {
+      if (isNaN(angka1) || isNaN(angka2)) {
         alert("Data yang diinput harus berupa angka!!");
       } else {
-        var total = a1 + a2; //rumus
+        var total = angka1 + angka2; //rumus
         formKalkulator.hasil.value = total; //penempatan hasil
       }
       break;
 
     case "button_kurang":
-      if (isNaN(a1) || isNaN(a2)) {
+      if (isNaN(angka1) || isNaN(angka2)) {
         alert("Data yang diinput harus berupa angka!!");
       } else {
-        var total = a1 - a2; //rumus
+        var total = angka1 - angka2; //rumus
         formKalkulator.hasil.value = total; //penempatan hasil
       }
       break;
 
     case "button_kali":
-      if (isNaN(a1) || isNaN(a2)) {
+      if (isNaN(angka1) || isNaN(angka2)) {
         alert("Data yang diinput harus berupa angka!!");
       } else {
-        var total = a1 * a2; //rumus
+        var total = angka1 * angka2; //rumus
         formKalkulator.hasil.value = total; //penempatan hasil
       }
       break;
 
     case "button_bagi":
-      if (isNaN(a1) || isNaN(a2)) {
+      if (isNaN(angka1) || isNaN(angka2)) {
         alert("Data yang diinput harus berupa angka!!");
       } else {
-        var total = a1 / a2; //rumus
+        var total = angka1 / angka2; //rumus
         formKalkulator.hasil.value = total; //penempatan hasil
       }
       break;
 
     case "button_pangkat":
-      if (isNaN(a1) || isNaN(a2)) {
+      if (isNaN(angka1) || isNaN(angka2)) {
         alert("Data yang di input harus berupa angka !!!");
       } else {
-        var total = Math.pow(a1, a2); //rumus
+        var total = Math.pow(angka1, angka2); //rumus
         formKalkulator.hasil.value = total; //penempatan hasil
       }
       break;
